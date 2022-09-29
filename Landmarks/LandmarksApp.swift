c4d816c17c002f05014a5eada29b6e653b23e651
abeaudoin <abeaudoin@ippon.fr>
2022-09-29 10:14:06 +0200

    init: landmarks

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ippon on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
